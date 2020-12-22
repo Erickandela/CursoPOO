d3850a5d87cc6e5a012b5e212f373ebbf54c2064
Erick Candela <erickandela_rocke@hotmail.com>
2020-12-22 15:46:07 -0600

    Se agregan metodos setter y getter en Java

--- Java/Main.java
+++ Java/Main.java
@@ -1,12 +1,12 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car = new Car("GLF342", new Account("Erick Candela", "ECAN32"));
-        car.passenger = 3;
-        car.printDataCar();
+        UberX uberX = new UberX("GLF342", new Account("Erick Candela", "ECAN32"), "Dodge", "Charger");
+        uberX.setPassenger(3);
+        uberX.printDataCar();
 
-        Car car2 = new Car("NLH1034", new Account("Jovani Arana", "JARA04"));
-        car2.passenger = 5;
-        car2.printDataCar();
+        // Car car2 = new Car("NLH1034", new Account("Jovani Arana", "JARA04"));
+        // car2.passenger = 5;
+        // car2.printDataCar();
     }
 }
