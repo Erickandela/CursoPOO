9591826ad95be5ac257eda3804b6e20cd7580fa4
Erick Candela <erickandela_rocke@hotmail.com>
2020-12-23 12:48:01 -0600

    Polimorfismo en Java

--- Java/Car.java
+++ Java/Car.java
@@ -1,8 +1,8 @@
 class Car{
-    Integer id;
-    String license;
-    Account driver;
-    private Integer passenger;
+    private Integer id;
+    private String license;
+    private Account driver;
+    protected Integer passenger;
 
     public Car(String license, Account driver){
         this.license = license;
@@ -25,7 +25,32 @@ class Car{
         }else {
             System.out.println("Se requiere la cantidad de 4 pasajeros.");
         }
-        
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Account driver) {
+        this.driver = driver;
+    }
+
+    
 
 }
--- Java/Main.java
+++ Java/Main.java
@@ -2,8 +2,13 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         UberX uberX = new UberX("GLF342", new Account("Erick Candela", "ECAN32"), "Dodge", "Charger");
-        uberX.setPassenger(3);
+        uberX.setPassenger(4);
         uberX.printDataCar();
+
+        UberVan uberVan = new UberVan("FDL5432", new Account ("Candela Erick", "INE"));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
+
 
         // Car car2 = new Car("NLH1034", new Account("Jovani Arana", "JARA04"));
         // car2.passenger = 5;
